6efe77ab79214746e7dabbc8018de1dd539e82fb
Tze-Yee Koh <tzeyeekoh@gmail.com>
2021-08-23 12:57:10 +0800

    Initial commit

--- StonkLSTM/data_query.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-import config
-
-def load_dataset():
-    Stk = yf.Ticker(config.ticker)
-    df_data = Stk.history(start= config.train_set_startdate)
-    df_data.reset_index(level=0, inplace=True)
-    return df_data
