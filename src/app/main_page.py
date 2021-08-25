8471905162e87c2cac5d5418b82942599b540b81
Tze-Yee Koh <tzeyeekoh@gmail.com>
2021-08-25 09:33:53 +0800

    Add select stock

--- src/app/main_page.py
+++ src/app/main_page.py
@@ -1,5 +1,7 @@
+from dash_html_components.Label import Label
 import pandas as pd
 import dash_html_components as html
+import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_table
 from dash.dependencies import Input, Output
@@ -19,16 +21,37 @@ navbar = dbc.NavbarSimple(
     dark=True,
 )
 
-index_content = html.Div(
-    id='index-container',
+ticker_selector = html.Div(
+    id='ticker_selector',
     className='content-container',
     children=[
-        html.H4("Stock gain prediction")
+        html.H4('Stock Price Insights'),
+        dbc.Row([
+            dbc.Col([
+                html.Label('Select Ticker'),
+                html.Br(),
+                dcc.Input(id='ticker', placeholder='Select Ticker')
+            ], width=6),
+            dbc.Col([
+                html.Label('Period'),
+                dcc.Dropdown(
+                    id='period',
+                    multi=False,
+                    options=[{'label':'Max', 'value':'Max'}, 
+                        {'label':'5-years', 'value':'5y'}, 
+                        {'label':'1-year', 'value':'1y'}, 
+                        {'label':'Year-to-date', 'value':'YTD'}, 
+                        {'label':'6-months', 'value':'6m'}],
+                    value='Max',
+                    clearable= False)
+            ], width=6)
+        ])
+        
     ])
 
 layout = html.Div([
     navbar,
     html.Br(),
-    index_content,
+    ticker_selector
 ])
 
